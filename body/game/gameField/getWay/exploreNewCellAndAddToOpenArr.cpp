a1b0949054ff47f1ec3c6f3ae14809a1dc93a0ef
lexyDiv <aledab@mail.ru>
2025-04-01 19:11:12 +0300

    before re way

--- body/game/gameField/getWay/exploreNewCellAndAddToOpenArr.cpp
+++ body/game/gameField/getWay/exploreNewCellAndAddToOpenArr.cpp
@@ -8,15 +8,25 @@
 // пересчитываем её значение F и изменяем указатель на родителя так, 
 // чтобы она указывала на текущую клетку.
 
+// [3, 1] уже находится в открытом списке, 
+// поэтому сравниваем её значение F со значением F таким, 
+// что если бы мы пришли на неё через текущую клетку. 
+// Это значения 60 и 64, соответственно, а значит, данные проверяемой клетки не нужно обновлять. 
+// Клетки с индексами [4, 1] и [4, 2] добавляем в открытый список, 
+// предварительно вычислив их значения величин G, H и F, 
+// а также установив указатель на родительскую клетку
+
 void GameField::exploreNewCellAndAddToOpenArr(ProtoObj *unit, ProtoObj *fatherCell, ProtoObj *potentialCell, ProtoObj *finishCell)
 {
   if (potentialCell->createCountData == this->createCount)
   {
     int G = this->get_G(fatherCell, potentialCell) + fatherCell->G;
-    if (potentialCell->G > G)
+    int F = G + potentialCell->H;
+    if (potentialCell->F >= F)
     {
       potentialCell->wayFather = fatherCell;
       potentialCell->G = G;
+      potentialCell->F = F;
     }
   }
   else if (
@@ -32,7 +42,6 @@ void GameField::exploreNewCellAndAddToOpenArr(ProtoObj *unit, ProtoObj *fatherCe
     potentialCell->H = H;
     potentialCell->F = potentialCell->G + potentialCell->H;
 
-    this->quickArr.push(potentialCell);
     this->openArr.push(potentialCell);
   }
 
--- body/game/gameField/getWay/getPotentialWay.cpp
+++ body/game/gameField/getWay/getPotentialWay.cpp
@@ -9,7 +9,6 @@ void GameField::getPotentialWay(ProtoObj *unit)
     ProtoObj *finishCell = unit->targetCell;
     ProtoObj *startCell = unit->cell;
 
-    hzCell = unit->targetCell;
 
     if (
         startCell &&
@@ -19,9 +18,6 @@ void GameField::getPotentialWay(ProtoObj *unit)
         this->createCount += 0.0000001;
         startCell->createCountData = this->createCount;
         this->openArr.clear();
-
-        this->quickArr.clear();
-        this->isQuick = false;
         this->min_F_cell = startCell;
 
         int iter = 0;
@@ -33,69 +29,21 @@ void GameField::getPotentialWay(ProtoObj *unit)
 
             MinData md;
 
-
             for (int i = 0; i < this->min_F_cell->aroundCells.length; i++)
             {
                 ProtoObj *pc = this->min_F_cell->aroundCells.getItem(i);
                 this->exploreNewCellAndAddToOpenArr(unit, this->min_F_cell, pc, finishCell);
-                if (unit->isPotentialWayComplite)
-                {
-                    break;
-                }
             }
 
-            // this->exp.clear();
-            // this->openArr.forEach([this](ProtoObj* cell, int i){
-            //     if (i % 3 == 0) {
-            //         Array<ProtoObj* > arr;
-            //         this->exp.push(arr);
-            //     }
-            //     this->exp.getItem3(this->exp.length - 1).push(cell);
-            // });
-
-            // MinData expM;
-            // for (int i = 0; i < this->exp.length; i++) {
-            //     Array<ProtoObj* > &arr = this->exp.getItem3(i);
-            //     for (int k = 0; k < arr.length; k ++) {
-            //         ProtoObj* cell = arr.getItem(k);
-            //         if (!expM.cell || expM.cell->F > cell->F) {
-            //             expM.cell = cell;
-            //             expM.i = i;
-            //             expM.k = k;
-            //         }
-            //     }
-            // }
-
-            //////////////////////////////////////////////////////////
-            // if (this->openArr.length)
-            // {
-            //     md = this->openArr.getMinData([](ProtoObj *cell)
-            //                                   { return cell->F; });
-            //     this->min_F_cell = md.cell;
-            //     this->openArr.splice(md.index, 1);
-            // }
-
-          bool brc = false;
-          int it = 0;
             for (int i = this->openArr.length - 1; i >= 0; i--)
             {
-                it ++;
                 ProtoObj *cell = this->openArr.getItem(i);
                 if (!md.cell || md.cell->F >= cell->F)
                 {
                     md.cell = cell;
                     md.index = i;
-                    if (it <= 8 && md.cell->F <= this->min_F_cell->F) {
-                        brc = true;
-                        console.log("quick");
-                        break;
-                    }
-                }
-                if (brc) {
-                    break;
                 }
             }
-
 
             // for (int i = 0; i < this->openArr.length; i++)
             // {
@@ -111,11 +59,6 @@ void GameField::getPotentialWay(ProtoObj *unit)
             this->openArr.splice(md.index, 1);
             ///////////////////////////////////////////////////////
 
-            // if (md.cell != expM.cell) {
-            //     console.log("!=");
-            //     console.log("md.cell->F = " + to_string(md.cell->F) + " expM.cell->F = " + to_string(expM.cell->F));
-            // }
-
             if (unit->isOnGetPotentialWayGetTarget(this->min_F_cell) ||
                 unit->isNeedReturnGetPotentialWay)
             {
@@ -123,22 +66,18 @@ void GameField::getPotentialWay(ProtoObj *unit)
                 {
                     this->potentialWayCreate(unit, this->min_F_cell);
                 }
-                // return;
                 break;
             }
 
             if (!this->quickArr.length && !this->openArr.length)
             {
                 unit->isPotentialWayComplite = true;
-            }
-            if (unit->isPotentialWayComplite)
-            {
                 break;
             }
         }
-        auto end_time = std::chrono::steady_clock::now();
-        auto res = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-         console.log(to_string(res.count()));
-       // console.log(to_string(iter));
+        // auto end_time = std::chrono::steady_clock::now();
+        // auto res = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+        //  console.log(to_string(res.count()));
+        // console.log(to_string(iter));
     }
 };
